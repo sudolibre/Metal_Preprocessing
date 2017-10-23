e3e5b6badf6a79cd7665acc71842a86cd0f30316
Jonathon Day <jonathon.day@stablekernel.com>
2017-10-23 15:24:33 -0400

    Initial Commit

---
+++ Metal_Preprocessing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metal_Preprocessing
+//
+//  Created by Jon Day on 10/23/17.
+//  Copyright © 2017 com.metal.preprocessing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
